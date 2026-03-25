f8451f1d8a87bd9b65561005354a4016f261e26e
ncallen18 <92344898+ncallen18@users.noreply.github.com>
2026-03-25 16:59:06 -0400

    updated for windows and apple

--- better_code.py
+++ better_code.py
@@ -2,15 +2,22 @@ import robot_control as rc
 
 REPO_IDS = {
     "skywalker": "SkywalkerLi/so101_03_21_26_demo1",
-    "nicole": "nc8304/so101_031626"
+    "nicole": "nc8304/so101_032326_white_back"
 }
+
+NICOLE_ROBOT = {**rc.ROBOT_DEFAULTS, "port": "COM5", "cameras": "{front: {type: opencv, index_or_path: 1, width: 640, height: 480, fps: 30}}"}
+NICOLE_TELEOP = {**rc.TELEOP_DEFAULTS, "port": "COM4"}
 
 def do_teleoperate():
     rc.teleoperate()
 
 
-def do_record(repo_id="nc8304/so101_v2", num_episodes=5, single_task="Testing", resume=True):
-    rc.record(repo_id=repo_id, num_episodes=num_episodes, single_task=single_task, resume=resume)
+def do_record(repo_id="nc8304/so101_v2", num_episodes=5, single_task="Testing", resume=False):
+    if repo_id == REPO_IDS["nicole"]:
+        rc.record(repo_id=repo_id, num_episodes=num_episodes, single_task=single_task, resume=resume,
+                  robot=NICOLE_ROBOT, teleop=NICOLE_TELEOP)
+    else:
+        rc.record(repo_id=repo_id, num_episodes=num_episodes, single_task=single_task, resume=resume)
 
 def do_replay(repo_id="nc8304/so101", episode=0):
     rc.replay(repo_id=repo_id, episode=episode)
@@ -18,5 +25,5 @@ def do_replay(repo_id="nc8304/so101", episode=0):
 
 if __name__ == "__main__":
     #do_teleoperate()
-    do_record(repo_id=REPO_IDS["skywalker"], num_episodes=100, single_task="1st Training ", resume=False) #if file exsists make new one
+    do_record(repo_id=REPO_IDS["nicole"], num_episodes=20, single_task="Nicole Data ", resume=False) #if file exsists make new one
     #do_replay(repo_id="nc8304/so101_031626",episode=0)
