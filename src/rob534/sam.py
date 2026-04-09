650671d00b2b12f2bad1fe9b46e6dbca360c59da
Kev-Park <kevy0403@gmail.com>
2026-04-08 20:39:25 -0400

    update sam prompt

--- src/rob534/sam.py
+++ src/rob534/sam.py
@@ -7,7 +7,7 @@ from PIL import Image
 from transformers import Sam3Processor, Sam3Model
 
 # --- Configuration ---
-TEXT_PROMPT = "the block being grasped"
+TEXT_PROMPT = "the block actively being grasped"
 VIDEO_INPUT = "file-000-h264.mp4"
 VIDEO_OUTPUT = "file-000-segmented.mp4"
 # Score threshold filters predicted instances before mask extraction.
